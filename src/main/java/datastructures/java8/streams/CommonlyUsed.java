09e6c481b855e123a09247e4cf8cc71bf332e9e4
Tarun27 <tarunkr27@gmail.com>
2023-01-20 22:25:37 +0530

    Create CommonlyUsed.java

---
+++ src/main/java/datastructures/java8/streams/CommonlyUsed.java
@@ -0,0 +1,25 @@
+class CommonlyUsed{
+
+public static void main(String args[]){
+    
+  List<String> list = Arrays.asList("3", "6", "8", "14", "15");
+  
+        // Using Stream mapToInt(ToIntFunction mapper)
+        // and displaying the corresponding IntStream
+        list.stream().mapToInt(num -> Integer.parseInt(num))
+                     .filter(num -> num % 3 == 0)
+                     .forEach(System.out::println);  
+  
+  //convert list of Integers to primitive int array
+int[] arr = list2.stream().mapToInt(Integer::intValue).toArray();
+  
+        // Creating object of Integer class inside main()
+        Integer intobject = new Integer(68);
+ 
+        // Returns the value of this Integer as an int
+        int i = intobject.intValue();
+
+
+}
+  
+}
