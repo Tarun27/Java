4d4d806b6f8022cd2c77fe00b0e8131631f36137
Tarun27 <tarunkr27@gmail.com>
2023-01-23 20:49:35 +0530

    Update and rename StreamFeatures to StreamFeatures.java

---
+++ src/main/java/datastructures/java8/streams/StreamFeatures.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.util.stream.*;
+public class MyClass {
+    public static void main(String args[]) {
+     List<Integer> list = List.of(1,4,7,5,3,8,6,9);
+     
+     //skip 1st no, print 2nd and 3rd numbers greater than 5
+     list.stream().filter(n->n>5)
+     .skip(1)
+     .limit(2)
+     .forEach(no->System.out.println(no));
+     
+     int res = list.stream().max((x,y)->Integer.compare(x,y)).get();
+     System.out.println(res);
+     
+     
+     res = list.stream().filter(n->n>5)
+     .findFirst()
+     .orElse(-1);
+     
+    System.out.println(res);
+        Person p1 = new Person("n1",5,"a");
+         Person p2 = new Person("n2",1,"b");
+         Person p3 = new Person("n3",8,"c");
+        
+        List<Person> p =  List.of(p1,p2,p3);
+        
+           p.stream().map(psn-> psn.name)
+          .forEach(name->System.out.println(name));
+          
+          
+          // another way of writing stream
+          Stream<Person> pStream = Stream.of(p1,p2,p3);
+          
+          
+        pStream.map(psn-> psn.name)
+          .forEach(name->System.out.println(name));
+          
+          
+        
+    }
+}
+
+class Person{
+    String name;
+    int age;
+    String address;
+    
+    Person(String name, int age, String address){
+        this.name=name;
+        this.age=age;
+        this.address=address;
+    }
+    
+}
