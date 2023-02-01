bd2b3074070942ada35a7747f161a4b6c2d7185e
Tarun27 <tarunkr27@gmail.com>
2023-02-01 19:05:48 +0530

    Create interview.java

---
+++ src/main/java/datastructures/java/interview.java
@@ -0,0 +1,14 @@
+public class MyClass {
+    public static void main(String args[]) {
+      Integer x=1110;
+      Integer y=1110;
+      int z=111;
+      int a=111;
+
+      System.out.println(x.equals(y)); // true
+      System.out.println(x == y); // false
+      System.out.println(z == a); // true
+
+
+    }
+}
