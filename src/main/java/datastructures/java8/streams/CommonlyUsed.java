cc596069cdc2025eb6bf28ceff5755fe02f7cd17
Tarun27 <tarunkr27@gmail.com>
2023-02-01 18:56:47 +0530

    Update CommonlyUsed.java

--- src/main/java/datastructures/java8/streams/CommonlyUsed.java
+++ src/main/java/datastructures/java8/streams/CommonlyUsed.java
@@ -23,7 +23,20 @@ int[] arr = list2.stream().mapToInt(Integer::intValue).toArray();
     List<Integer> listWithoutDuplicates = listWithDuplicates.stream()
      .distinct()
      .collect(Collectors.toList());
+    
+        List<Integer> duplicatesList = listWithDuplicates.stream()
+            .filter(i->Collections.frequency(listWithDuplicates,i) > 1)
+            .collect(Collectors.toList());                           
 
+Integer[] numbers = new Integer[] { 1, 2, 1, 3, 4, 4 };
+Set<Integer> allItems = new HashSet<>();
+Set<Integer> duplicates = Arrays.stream(numbers)
+        .filter(n -> !allItems.add(n)) //Set.add() returns false if the item was already in the set.
+        .collect(Collectors.toSet());
+System.out.println(duplicates); // [1, 4]
+    
+    
+    
 }
   
 }
