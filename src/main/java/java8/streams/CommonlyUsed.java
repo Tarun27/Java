6dd4041bcce8d9bec73acb6a78c419e214b9d0f4
Tarun27 <tarunkr27@gmail.com>
2023-02-21 23:23:08 +0530

    Update CommonlyUsed.java

--- src/main/java/java8/streams/CommonlyUsed.java
+++ src/main/java/java8/streams/CommonlyUsed.java
@@ -34,8 +34,27 @@ Set<Integer> duplicates = Arrays.stream(numbers)
         .filter(n -> !allItems.add(n)) //Set.add() returns false if the item was already in the set.
         .collect(Collectors.toSet());
 System.out.println(duplicates); // [1, 4]
-    
-    
+    //int[] -> IntStream -> Stream<Integer> -> Integer[]
+        int[] num = {3, 4, 5};
+
+        //1. int[] -> IntStream
+        IntStream stream = Arrays.stream(num); //   IntStream.of(array);
+
+        //2. IntStream -> Stream<Integer>
+        Stream<Integer> boxed = stream.boxed();
+
+        //3. Stream<Integer> -> Integer[]
+        Integer[] result = boxed.toArray(Integer[]::new);
+
+        System.out.println(Arrays.toString(result));
+
+        // one line
+        Integer[] oneLineResult = Arrays.stream(num).boxed().toArray(Integer[]::new);
+        System.out.println(Arrays.toString(oneLineResult));
+ //   convert primitive array to stream
+        
+
+       
     
 }
   
