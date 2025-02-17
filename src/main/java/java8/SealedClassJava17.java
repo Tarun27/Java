9e3aafd385b9f0179c681168e5d0393a8f3df32d
Tarun27 <tarunkr27@gmail.com>
2025-02-18 01:10:45 +0530

    Create SealedClassJava17.java

---
+++ src/main/java/java8/SealedClassJava17.java
@@ -0,0 +1,50 @@
+// Sealed class
+public sealed abstract class Shape permits Circle, Rectangle, Triangle {
+    public abstract double area();
+}
+
+// Sealed subclass for Circle
+ final class Circle extends Shape {
+    private final double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+}
+
+// Sealed subclass for Rectangle
+ final class Rectangle extends Shape {
+    private final double length;
+    private final double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double area() {
+        return length * width;
+    }
+}
+
+// Non-sealed subclass for Triangle
+ non-sealed class Triangle extends Shape {
+    private final double base;
+    private final double height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    public double area() {
+        return 0.5 * base * height;
+    }
+}
